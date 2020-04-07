1f50b8b8d79deac51fed6ff461c8c86a6c0a5252
gaoyuanyuan920 <yzxl4321@163.com>
2020-04-07 16:05:48 +0800

    Initial commit

---
+++ app/src/main/java/com/example/lenovo/myapplication/Ch1Activity.java
@@ -0,0 +1,13 @@
+package com.example.lenovo.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Ch1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ch1);
+    }
+}
---
+++ app/src/main/java/com/example/lenovo/myapplication/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.lenovo.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.work1);
+        //Button button1 = (Button) findViewById(R.id.work2);
+        class myclass implements View.OnClickListener {
+            @Override
+            public void onClick(View view) {
+                //界面跳转
+                Intent intent =new Intent(MainActivity.this, Ch1Activity.class);
+                startActivity(intent);
+
+            }
+        }
+        Button button1 = (Button) findViewById(R.id.work2);
+        button1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                //界面跳转
+                Intent intent=new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    }
+
